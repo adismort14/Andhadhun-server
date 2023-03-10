d090d7e6ad12d27d19c8481c98f10e466805ca23
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-03-10 15:34:36 +0530

    python code bug fixing and changing the port

--- arduinoData.py
+++ arduinoData.py
@@ -5,14 +5,15 @@ import sys
 amount = int(sys.argv[1])
 
 # from serial.tools import list_ports
+
 # port = list(list_ports.comports())
 # for p in port:
-#    print(p.device)
+#     print(p.device)
 
 print(f"Reached the py file and the data is {amount}")
-serial = Serial("/dev/cu.usbmodem11401", 9600)
+serial = Serial("COM5", 9600)
 # Enter your port
 
 time.sleep(2)
 # print("Reached the py file")
-serial.write(bytes(str(amount)), encoding="utf-8")
+serial.write(str(amount).encode("utf-8"))
