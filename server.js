851eb68c7052ddebe9ee50fa9e2361fb108abcb7
Aditya Sankhla <adityasa@iitbhilai.ac.in>
2023-03-10 15:53:11 +0530

    used python-shell library and removed child process spawning

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { spawn } = require("child_process");
+const { PythonShell } = require("python-shell");
 
 const app = express();
 const port = 5000;
@@ -11,7 +12,19 @@ app.use(bodyParser.json());
 app.post("/api/data", (req, res) => {
   const data = req.body.data;
   console.log("Received data:", data);
-  const pythonProcess = spawn("python", ["./serial.py", data]);
+  let options = {
+    mode: "text",
+    pythonOptions: ["-u"],
+    scriptPath: "./",
+    args: [data],
+  };
+
+  PythonShell.run("./arduinoData.py", options, function (err, results) {
+    if (err) throw err;
+    console.log("Python script executed successfully:", results);
+    res.send("Data processed successfully");
+  });
+  // const pythonProcess = spawn("python", ["./serial.py", data]);
 
   // pythonProcess.stdout.on("data", function (data) {
   //   console.log(data.toString());
@@ -29,7 +42,6 @@ app.post("/api/data", (req, res) => {
   //       res.status(500).send("Failed to process data");
   //     }
   //   });
-  res.send("Data processed successfully");
 });
 
 app.listen(port, () => {
